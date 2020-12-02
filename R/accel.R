1d901d1aa45bbcaa3d74c0e0d778757d6e5f3c23
Daniel Russ <druss@nih.gov>
2020-12-02 16:08:08 -0500

    initial version

---
+++ R/accel.R
@@ -0,0 +1,11 @@
+#' Simple Hello World function...
+#'
+#' 
+#' @examples 
+#' helloWorld()
+#'
+#' @return the string 'hello world' 
+#' @export
+helloWorld<-function(){
+  return('hello world')
+}
---
+++ R/ref_data.R
@@ -0,0 +1,19 @@
+#' Reference data for Accelerometer testing
+#'
+#'
+#' @docType data
+#'
+#' @usage data(ref_data)
+#'
+#' @format An object of class \code{"tibble"}
+#'
+#' @keywords datasets
+#'
+#' @references 
+#' https://github.com/activityMonitoring/biobankAccelerometerAnalysis
+#'
+#'
+#' @examples
+#' 
+#' ref_data[1:5,1:6]
+"ref_data"
